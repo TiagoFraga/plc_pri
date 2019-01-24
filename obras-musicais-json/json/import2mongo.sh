3c3a27ccc8b1e535f4fa7262ffba343a6ea206ef
TiagoFraga <t.fraga94@hotmail.com>
2019-01-24 16:26:24 +0000

    removi a pasta obras json

--- obras-musicais-json/json/import2mongo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# make sure you always put $f in double quotes to avoid any nasty surprises i.e. "$f"
-for f in $*
-do
-  echo "Processing $f file..."
-  mongoimport -h localhost:27017 -d amd -c obras "$f" 
-done
