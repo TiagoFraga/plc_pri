34d76a0e89a0327fbfd997061d38e1628b07fd9f
JoaoGomes2110 <joaopedro2110@hotmail.com>
2019-01-30 16:42:02 +0000

    Atualizada listagem de obras

--- iBanda/routes/admin.js
+++ iBanda/routes/admin.js
@@ -25,12 +25,31 @@ router.get('/users/listar',passport.authenticate('isAdmin',{session:false}),(req
 
 router.post('/users/registar',passport.authenticate('isAdmin',{session:false}),(req, res) => {
     axios.post('http://localhost:9009/api/admin/users/registar', req.body)
-        .then(()=> res.redirect('http://localhost:9009/admin'))
+        .then(()=> res.redirect('http://localhost:9009/admin/users/listar'))
         .catch(erro => {
             console.log('Erro na inserção do Utilizador: ' + erro)
             res.render('error', {error: erro, message: "Erro na inserção de Utilizador"})
     })
 })
+
+router.get('/obras/listar',passport.authenticate('isAdmin',{session:false}),(req, res) => {
+    axios.get('http://localhost:9009/api/admin/obras/listar')
+        .then(dados => {res.render('listarObras',{obras: dados.data})})
+        .catch(erro => {
+            console.log('Erro na listagem do Utilizador: ' + erro)
+            res.render('error', {error: erro, message: "Erro na listagem de Utilizadores"})
+    })
+})
+
+router.get('/obras/listar/obra/:obra',passport.authenticate('isAdmin',{session:false}),(req, res) => {
+    axios.get('http://localhost:9009/api/admin/obras/listar/obra/' + req.params.obra)
+        .then(dados => {res.render('listarObra',{obra: dados.data})})
+        .catch(erro => {
+            console.log('Erro na listagem do Utilizador: ' + erro)
+            res.render('error', {error: erro, message: "Erro na listagem de Utilizadores"})
+    })
+})
+
 
 router.get('/noticias/registar',passport.authenticate('isAdmin',{session:false}),(req, res) => {
     res.render('registarNoticia')
